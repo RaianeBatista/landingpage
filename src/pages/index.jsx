85b67772ccea61a3de6ad67e82603dd1221d3906
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-08-13 09:59:42 -0300

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/index.jsx";
+import styles from  "../styles/Home.module.scss";
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
